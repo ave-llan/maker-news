a2e162560fe1de64dd383b8375a4cabb3ec58348
John Leszczynski <jrleszczynski@gmail.com>
2015-11-26 07:43:12 -0500

    create basic express app

---
+++ app/index.js
@@ -0,0 +1,20 @@
+var express        = require('express')
+  // , passport       = require('passport')
+  // , GitHubStrategy = require('passport-github').GitHubStrategy
+
+var app = express()
+
+// app.use(express.logger())
+// app.use(express.cookieParser())
+// app.use(express.bodyParser())
+// app.use(express.methodOverride())
+// app.use(express.session({ secret: 'cookie monster' }))
+// app.use(passport.initialize())
+// app.use(passport.session())
+// app.use(app.router)
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(process.env.PORT || 5000)
